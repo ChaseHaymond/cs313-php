0d3b718e7a3aad8b5026fcb13a27b9dd5d4c4ed9
Chase Haymond <hay14010@byui.edu>
2019-05-04 15:05:45 -0600

    fix bugs

--- web/02Prove/home.php
+++ web/02Prove/home.php
@@ -64,7 +64,7 @@
 	<br>About Me <hr> I am from Arvada, Colorado. I was born and raised there. I am studying computer science. If everything goes acording to plan, I should graduate next winter. I like web development but I am not the best at design. To the left are some pictures of things I like to do.
     
 	</div>
-    <br><hr>
+    <br>
     <div id="footer">
          <?php include 'title.php';?>
     </div>
