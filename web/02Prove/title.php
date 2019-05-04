580352bc92bc043d8ce7f35f215cba0ab666f164
Chase Haymond <hay14010@byui.edu>
2019-05-04 14:30:55 -0600

    fix bugs

--- web/02Prove/title.php
+++ web/02Prove/title.php
@@ -1,15 +1,3 @@
 <?php
-
-	echo "<h1>
-        
-	Welcome to Chase Haymond's Website 
-        
-	<hr> 
-        
-	<a id="pageLink" href="home.php"> Home Page </a> 
-        
-	<a id="pageLink" href="assignments.php"> Assignments Page </a> 
-    
-	</h1>";
-
+	echo "test";
 ?>
