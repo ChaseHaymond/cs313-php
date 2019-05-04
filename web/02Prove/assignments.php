1080338ca7aaa7ebaca625fdb3574585b2381905
Chase Haymond <hay14010@byui.edu>
2019-05-04 14:19:47 -0600

    php

--- web/02Prove/assignments.php
+++ web/02Prove/assignments.php
@@ -9,8 +9,8 @@
     <h1>
         Welcome to Chase Haymond's Website 
         <hr> 
-        <a id="pageLink" href="index.html"> Home Page </a> 
-        <a id="pageLink" href="assignments.html"> Assignments Page </a>  
+        <a id="pageLink" href="home.php"> Home Page </a> 
+        <a id="pageLink" href="assignments.php"> Assignments Page </a>  
 
     </h1>
     
