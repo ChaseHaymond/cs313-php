947f919bf89a8a1b05220b0ce5f4eb7b6fda98ef
Chase Haymond <hay14010@byui.edu>
2019-05-04 13:40:41 -0600

    Add project 2

---
+++ web/02Prove/main.js
@@ -0,0 +1,20 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.  Enjoy!
+ */
+var snowboardPicture = document.getElementById("snowboard");
+
+function snowboard() {
+    //alert("a");
+    document.getElementById("image").src="snowbaording.jpg";
+}
+
+function ramen() {
+    //alert("b");
+    document.getElementById("image").src="ramen2.jpg";
+}
+
+function piano() {
+    //alert("c");
+    document.getElementById("image").src="piano.jpg";
+}
+
