56b41b5211542a0b3294b6d1ae65f22a77ac5566
Chase Haymond <hay14010@byui.edu>
2019-05-08 21:41:33 -0600

    Add team 03 activity

--- web/03Team/info.php
+++ web/03Team/info.php
@@ -1,5 +1,1 @@
-<?php echo "User Name: " . $_POST["name"] . "<br>"; 
-	echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
-	echo "Major: " . $_POST["major"] . "<br>";
-	echo "Comment: " . $_POST["comment"] . "<br>";
-	?>
+<?php echo "User Name: " . $_POST["name"] . "<br>"; ?>
