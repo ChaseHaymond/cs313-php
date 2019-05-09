7efdd7cfc9fc4d1b15ec243ee78aed08e27d8b84
Chase Haymond <hay14010@byui.edu>
2019-05-08 22:15:41 -0600

    Add team 03 activity

--- web/03Team/info.php
+++ web/03Team/info.php
@@ -2,10 +2,11 @@
 
     $email = $_POST["email"];
 
-echo "<a href=\"mailto: $email\"> test </a>"
+//echo "<a href=\"mailto: $email\"> test </a>"
 
     echo "User Name: " . $_POST["name"] . "<br>"; 
 	//echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
+    echo "<a href='".$link_address."'>Mail To:</a>";
     echo "Email: " . $_POST["email"] . "<br>";
 	echo "Major: " . $_POST["major"] . "<br>";
 	echo "Comment: " . $_POST["comment"] . "<br>";
