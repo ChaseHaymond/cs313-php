d57ebbfad54e85b1fbc8166155e2dffbf83aa4c3
Chase Haymond <hay14010@byui.edu>
2019-05-08 22:11:32 -0600

    Add team 03 activity

--- web/03Team/info.php
+++ web/03Team/info.php
@@ -1,6 +1,11 @@
 <?php 
+
+    $email = $_POST["email"];
+
+<a href="mailto: $email"> test </a>
+
     echo "User Name: " . $_POST["name"] . "<br>"; 
-//	echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
+	//echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
     echo "Email: " . $_POST["email"] . "<br>";
 	echo "Major: " . $_POST["major"] . "<br>";
 	echo "Comment: " . $_POST["comment"] . "<br>";
