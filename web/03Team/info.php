8533a52666b296371e79eb539fe40aa057adf497
Chase Haymond <hay14010@byui.edu>
2019-05-08 22:04:33 -0600

    Add team 03 activity

--- web/03Team/info.php
+++ web/03Team/info.php
@@ -1,7 +1,4 @@
 <?php 
-    if (isset($_POST["major"])) {
-        $major = $_POST["major"]
-    }
     echo "User Name: " . $_POST["name"] . "<br>"; 
 //	echo "Email: " . "<a href=\"mailto:" . $_POST["email"]> . "\">" . $_POST["email"] . "</a><br>";
 	echo "Major: " . $major . "<br>";
