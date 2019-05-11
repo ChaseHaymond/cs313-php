f3f7b18578ebacede7aa3a438cf39d50af86e97c
Chase Haymond <hay14010@byui.edu>
2019-05-11 17:53:33 -0600

    Add prove 03

--- web/03Prove/items.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function addRamen() {
-//        $_SESSION[cart]=array_diff($_SESSION[cart],"ramen");
-        echo "<script type='text/javascript'>alert('Clicked Ramen');</script>";
-    }
-?> 
