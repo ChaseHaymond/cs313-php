57693c848d4f8c568cf594be893cbf6532d47559
Chase Haymond <hay14010@byui.edu>
2019-05-11 17:29:17 -0600

    Add prove 03

--- web/03Prove/home.php
+++ web/03Prove/home.php
@@ -60,15 +60,7 @@ session_start();
              <td id="add"><button>Add To Cart</button></td>
          </tr>
     </table> 
-    </p>
-    
-        <?php
-    function addRamen() {
-//        $_SESSION[cart]=array_diff($_SESSION[cart],"ramen");
-        echo "<script type='text/javascript'>alert('Clicked Ramen');</script>";
-    }
-?> 
-
     
 </body>
+
 </html>
