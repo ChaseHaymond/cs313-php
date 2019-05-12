77569337dfedb13b2049f299658e300384e0f8c9
Chase Haymond <hay14010@byui.edu>
2019-05-11 20:56:29 -0600

    Add prove 03

--- web/03Prove/addItem.php
+++ web/03Prove/addItem.php
@@ -19,25 +19,28 @@ session_start();
 //        array_push($_SESSION['item'],$_GET['item']);
         if ($_GET['item'] == "glasses") {
             $_SESSION["glasses"]++;
-            echo $_SESSION["glasses"] . "<br>";
+            //echo $_SESSION["glasses"] . "<br>";
         }
         
         if ($_GET['item'] == "ramen") {
             $_SESSION["ramen"]++;
-            echo $_SESSION["ramen"] . "<br>";
+            //echo $_SESSION["ramen"] . "<br>";
         }
         
         if ($_GET['item'] == "shoes") {
             $_SESSION["shoes"]++;
-            echo $_SESSION["shoes"] . "<br>";
+            //echo $_SESSION["shoes"] . "<br>";
         }
         
         if ($_GET['item'] == "toothbrush") {
             $_SESSION["toothbrush"]++;
-            echo $_SESSION["toothbrush"] . "<br>";
+            //echo $_SESSION["toothbrush"] . "<br>";
         }
         
-        echo $_SESSION["ramen"] = 0;
+        echo $_SESSION["glasses"] . "<br>";
+        echo $_SESSION["ramen"] . "<br>";
+        echo $_SESSION["shoes"] . "<br>";
+        echo $_SESSION["toothbrush"] . "<br>";
         
     ?>
     </body>
