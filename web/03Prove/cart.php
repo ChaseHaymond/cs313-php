bfc34b173a0b33ed855ef893edc052459c3f1b35
Chase Haymond <hay14010@byui.edu>
2019-05-11 20:03:24 -0600

    Add prove 03

--- web/03Prove/cart.php
+++ web/03Prove/cart.php
@@ -11,6 +11,23 @@
 	<a id="pageLink" href="cart.php"> View Cart </a> 
     </h1>
     
+    
+    <?php        
+        echo $_GET['item'] . "<br>";    
+        
+        array_push($_SESSION['item'],$_GET['item']);
+        
+        print_r( $_SESSION['item']);
+            
+        echo "<br>";
+        
+        foreach($_SESSION['item'] as $x => $x_value) {
+            echo $x_value;
+            echo "<br>";
+        }
+    ?>
+    
+<!--
     <p id="content">
         Contents of your cart <br><br>
      <table>
@@ -31,6 +48,7 @@
          
     </table> 
     </p>
+-->
     
 </body>
 </html>
