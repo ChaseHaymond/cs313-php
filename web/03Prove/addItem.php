f9c3998a7c73621608082878635deeff1674492b
Chase Haymond <hay14010@byui.edu>
2019-05-11 21:04:41 -0600

    Add prove 03

--- web/03Prove/addItem.php
+++ web/03Prove/addItem.php
@@ -34,8 +34,7 @@ session_start();
         echo $_SESSION["shoes"] . "<br>";
         echo $_SESSION["toothbrush"] . "<br>";
         
-        header("./home.php"); /* Redirect browser */
-        exit();
+
         
     ?>
     </body>
