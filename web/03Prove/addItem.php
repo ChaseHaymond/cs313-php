6d48e58d0e244e3a0cdd1c0a3eb127d75027f482
Chase Haymond <hay14010@byui.edu>
2019-05-11 18:23:06 -0600

    Add prove 03

--- web/03Prove/addItem.php
+++ web/03Prove/addItem.php
@@ -7,20 +7,9 @@ session_start();
     
     
     <?php 
-        
-        $_SESSION['test'] = "test";
-        
         $_SESSION['item'] = $_GET['item'];
         
-        echo $_SESSION['item']; 
-        
-        echo "Favorite local is " . $_SESSION["test"] . ".<br>";
-        echo "Favorite session is " . $_SESSION["item"] . ".<br>";
-        
-        
-        $message = "test";
-echo "<script type='text/javascript'>alert('$message');</script>";
-        
+        echo $_SESSION['item'];      
         
         ?>
     </body>
