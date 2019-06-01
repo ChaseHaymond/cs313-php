ec21527fb1240bda918f571c6d1cf314f176021b
Chase Haymond <hay14010@byui.edu>
2019-06-01 17:47:12 -0600

    allow updating index

--- web/06Prove/insertBook.php
+++ web/06Prove/insertBook.php
@@ -10,6 +10,45 @@ include("dbconection.php");
 <body >
 
 <?php
+
+    $title = $_GET['title'];
+    $author = $_GET['author'];
+    $genre = $_GET['genre'];
+    $sdate = $_GET['sdate'];
+    $edate = $_GET['edate'];
+    
+    $authorId = 0;
+    $genreId = 0;
+    
+    foreach ($db->query('SELECT * FROM authors') as $row) {
+        $dbName = $row['firstname'] . " " . $row['lastname'];
+        if(strtoupper($dbName) == strtoupper($author)) {
+            $authorId = $row['id'];
+        }
+    }
+    
+    foreach ($db->query('SELECT * FROM genres') as $row) {
+        $dbName = $row['genre']
+        if(strtoupper($dbName) == strtoupper($genre)) {
+            $genreId = $row['id'];
+        }
+    }
+    
+    if(authorId == 0) {
+        $db = get_db();
+    
+        $stmt = $db->prepare('INSERT INTO authore(firstName) VALUES(' . $author . ');');
+        $stmt->exicute();
+    }
+    
+    $authorId = $pdo->lastInsertId('authors_id_seq');
+    
+    $db = get_db();
+    
+    $stmt = $db->prepare('INSERT INTO books(name, author, genre) VALUES(' . $title . ', ' . $authorId . ', ' . $genreId . ');');
+    $stmt->exicute();
+    
+    
 $search = $_GET['search']; 
 $searchType = $_GET['searchType'];
     
