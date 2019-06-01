be63b595777f73222134e2ca02b5da5a896258e6
Chase Haymond <hay14010@byui.edu>
2019-06-01 17:49:34 -0600

    allow updating index

--- web/06Prove/insertBook.php
+++ web/06Prove/insertBook.php
@@ -48,76 +48,11 @@ include("dbconection.php");
     $stmt = $db->prepare('INSERT INTO books(name, author, genre) VALUES(' . $title . ', ' . $authorId . ', ' . $genreId . ');');
     $stmt->exicute();
     
-    
-$search = $_GET['search']; 
-$searchType = $_GET['searchType'];
-    
-if($_GET['showAll']){
-    $query = "SELECT * FROM books AS b
-        JOIN authors AS a ON a.id = b.author_id
-        JOIN history AS h ON h.book_id = b.id";
+    $newPage = "index.php";
+    header("Location: $newPage");
+    die();
     
     
-    foreach ($db->query($query) as $row) {
-        echo '<strong>Title: </strong> ' . $row['name'] . 
-             '<strong>, Author: </strong> ' . $row['firstname'] . " " . $row['lastname'] .
-             '<strong>, Date Started: </strong> ' . $row['startdate'] .
-             '<strong>, Date Finished: </strong> ' . $row['enddate'] .
-             '&nbsp;';
-        
-        echo '</p><br>';
-    }
-    
-} else if($searchType == 'books'){
-    $query = "SELECT * FROM books AS b
-        JOIN authors AS a ON a.id = b.author_id
-        JOIN history AS h ON h.book_id = b.id
-        WHERE LOWER(name)=" ."LOWER('" . $search ."')";
-    
-    
-    foreach ($db->query($query) as $row) {
-        echo '<strong>Title: </strong> ' . $row['name'] . 
-             '<strong>, Author: </strong> ' . $row['firstname'] . " " . $row['lastname'] .
-             '<strong>, Date Started: </strong> ' . $row['startdate'] .
-             '<strong>, Date Finished: </strong> ' . $row['enddate'] .
-             '&nbsp;';
-        
-        echo '</p><br>';
-    }
-} else if($searchType == 'authors'){
-    $query = "SELECT * FROM books AS b
-        JOIN authors AS a ON a.id = b.author_id
-        JOIN history AS h ON h.book_id = b.id
-        WHERE LOWER(lastname)=" ."LOWER('" . $search ."')";
-    
-    
-    foreach ($db->query($query) as $row) {
-        echo '<strong>Title: </strong> ' . $row['name'] . 
-             '<strong>, Author: </strong> ' . $row['firstname'] . " " . $row['lastname'] .
-             '<strong>, Date Started: </strong> ' . $row['startdate'] .
-             '<strong>, Date Finished: </strong> ' . $row['enddate'] .
-             '&nbsp;';
-        
-        echo '</p><br>';
-    }
-}
-    
-    
-if($_GET['showAll']){showAll();}
-    
-function showAll() {
-    $query = "SELECT * 
-            FROM books 
-            WHERE LOWER(name)=" ."LOWER('" . $search ."')";
-    foreach ($db->query($query) as $row) {
-        echo '<strong>Title: </strong> ' . $row['name'] . 
-             '<strong>, Author: </strong> ' . $row['firstName'] . " " . $row['lastName'] .
-             '<strong>, Date Started: </strong> ' . $row['starteDate'] .
-             '<strong>, Date Finished: </strong> ' . $row['endDate'] .
-             '&nbsp;';
-        echo '</p><br>';
-    }
-}
 ?>
 </body>
 </html>
