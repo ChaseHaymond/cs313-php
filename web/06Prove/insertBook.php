39ada7e0a9e46aec4992b78cad6a1afdfffb0311
Chase Haymond <hay14010@byui.edu>
2019-06-01 18:04:40 -0600

    allow updating index

--- web/06Prove/insertBook.php
+++ web/06Prove/insertBook.php
@@ -34,12 +34,12 @@ include("dbconection.php");
         }
     }
     
-//    if($authorId == 0) {
-//        $db = get_db();
-//    
-//        $stmt = $db->prepare('INSERT INTO authors(firstName) VALUES(' . $author . ');');
-//        $stmt->exicute();
-//    }
+    if($authorId == 0) {
+        $db = get_db();
+    
+        $stmt = $db->prepare('INSERT INTO authors(firstName) VALUES(' . $author . ');');
+        $stmt->exicute();
+    }
 //    
 //    $authorId = $pdo->lastInsertId('authors_id_seq');
 //    
