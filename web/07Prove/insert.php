ebd546fbc6bc939fae3e3dab1d97f0b7d9bb125b
Chase Haymond <hay14010@byui.edu>
2019-06-05 17:56:41 -0600

    added table to show all list

--- web/07Prove/insert.php
+++ web/07Prove/insert.php
@@ -6,8 +6,18 @@ include("dbconection.php");
 <html lang="en">
 <head>
   <title>Chase Haymond Prove06</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body >
+    
+    
+            <h1>Welcome to Your Reading History <br>
+        <a id="pageLink" href="./index.php"> Search Your History </a> 
+        &nbsp;-&nbsp;
+        <a id="pageLink" href="./insert.php"> Add New Boook </a> 
+        </h1>
+    
+    
     <strong>Insert a new book</strong><br><br>
     <form action="./insertBook.php" method="posrt">
         
