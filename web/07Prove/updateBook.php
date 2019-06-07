36f4a31e576ed274a7fc444580a5e3f7c3b9a2e3
Chase Haymond <hay14010@byui.edu>
2019-06-07 11:15:31 -0600

    Working on updating

---
+++ web/07Prove/updateBook.php
@@ -0,0 +1,79 @@
+<?php
+include("dbconection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Chase Haymond Prove07</title>
+</head>
+<body >
+
+<?php
+
+    $title = $_GET['title'];
+    $fName = $_GET['fname'];
+    $lName = $_GET['lname'];
+    $genre = $_GET['genre'];
+    $sdate = $_GET['sdate'];
+    $edate = $_GET['edate'];
+    $histId = $_GET['histId'];
+    
+    $authorId = 0;
+    $genreId = 0;
+    
+    foreach ($db->query('SELECT * FROM authors') as $row) {
+        $dbName = $row['firstname'] . " " . $row['lastname'];
+        if(strtoupper($dbName) == strtoupper($fname . $lname)) {
+            $authorId = $row['id'];
+        }
+    }
+    
+    foreach ($db->query('SELECT * FROM genres') as $row) {
+        $dbName = $row['genre'];
+        if(strtoupper($dbName) == strtoupper($genre)) {
+            $genreId = $row['id'];
+        }
+    }
+    
+    
+    
+    
+    $query = "SELECT * FROM books AS b
+        JOIN authors AS a ON a.id = b.author_id
+        JOIN history AS h ON h.book_id = b.id
+        WHERE history_id=" . $_GET['id'];
+    
+    
+    foreach ($db->query($query) as $row) {
+        $query = 'INSERT INTO authors (firstName) VALUES (:firstName)'; 
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(':firstName', $author, PDO::PARAM_STR);
+        $stmt->execute();
+        
+        
+        $query = 'UPDATE authors SET firstname = ' . $fName . ',  lastname = ' . $lName . 'WHERE author_id = ' . $row['author_id']; 
+        $stmt = $db->prepare($query);
+        //$stmt->bindValue(':firstName', $author, PDO::PARAM_STR);
+        $stmt->execute();
+        
+        
+//        UPDATE Customers
+//        SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+//        WHERE CustomerID = 1;
+    }
+    
+    
+    
+    echo 'ns - ' . $newSdate . ', ne - ' . $newEdate;
+    
+    
+    $newPage = "./index.php";
+    header("Location: $newPage");
+    die();
+
+    
+    
+?>
+</body>
+</html>
