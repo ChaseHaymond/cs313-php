5c733036ea828273e1ccb6647070bb2ccd96d0a7
Chase Haymond <hay14010@byui.edu>
2019-06-06 22:00:05 -0600

    Working on updating

--- web/07Prove/update.php
+++ web/07Prove/update.php
@@ -19,6 +19,46 @@ include("dbconection.php");
     
     
     <strong>Update book's info</strong><br><br>
-?>
+    
+    
+    
+    
+    
+    
+    <?php
+    $query = "SELECT * FROM books AS b
+        JOIN authors AS a ON a.id = b.author_id
+        JOIN history AS h ON h.book_id = b.id
+        WHERE LOWER(history_id)=" ."LOWER('" . $_GET['id'];  ."')";
+    
+    
+    echo '<table>';
+    echo '<tr>';
+    echo '<th id="title">Title</th>';
+    echo '<th>Author</th>';
+    echo '<th>Start Date</th>';
+    echo '<th>End Date</th>';
+    echo '</tr>';
+
+    
+    foreach ($db->query($query) as $row) {
+        echo '<tr>';
+        
+        echo '<td>' . $row['name'] . '</td>' .
+             '<td>' . $row['firstname'] . " " . $row['lastname'] . '</td>' .
+             '<td>' . $row['startdate'] . '</td>' .
+             '<td>' . $row['enddate'] . '</td>' .
+             '<td>' . '<button id="button" name="button" onClick=\'location.href="./update.php?id=' . $row['history_id'] . '"\'>Edit</button>' . '</td>';
+
+        echo '</tr>';
+    }
+    
+    echo '</table>';
+    
+    ?>
+    
+    
+    
+    
 </body>
 </html>
